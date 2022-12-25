1128cd44661c9fdf5266ec37511dd8af44936a73
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-25 05:32:21 -0800

    HomeWork

---
+++ HW/TAsk01/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 1: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по
+//  убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void PrintMAtrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
+
+int[,] CreateMatrix(int m)
+{
+
+    int[,] matr = new int[m, m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+
+            matr[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+    return matr;
+
+}
+
+int[,] StreamLine(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int l = 0; l < arr.GetLength(1) - 1; l++)
+            {
+                if (arr[i, l] < arr[i, l + 1])
+                {
+                    int bigger = arr[i, l + 1];
+                    arr[i, l + 1] = arr[i, l];
+                    arr[i, l] = bigger;
+                }
+            }
+
+        }
+        
+
+        
+    }
+    return arr;
+}
+
+int[,] matrix = CreateMatrix(5);
+PrintMAtrix(matrix);
+PrintMAtrix(StreamLine(matrix));
---
+++ HW/Task02/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+// находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+void PrintMAtrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
+
+int[,] CreateMatrix(int m, int n)
+{
+
+    int[,] matr = new int[m, n];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+            matr[i, j] = new Random().Next(1, 10);
+
+        }
+    }
+    return matr;
+
+}
+
+void MinSumElementsOnRows(int[,] array)
+{
+    int sum = 0;
+    int minsum = 1000;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+        }
+        
+    }
+    Console.WriteLine($"Minimal sum of elements on rows {minsum}");
+    
+}
+
+
+
+int[,] matrix = CreateMatrix(8, 5);
+PrintMAtrix(matrix);
+MinSumElementsOnRows(matrix);
