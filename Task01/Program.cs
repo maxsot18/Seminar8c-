67b15f80003b6b49f939561ae127a4ef89ae828b
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-24 22:58:26 -0800

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами 
+// первую и последнюю строку массива.
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+
+            // matr[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangePlaces(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int lastrow = matr.GetLength(0) - 1;
+        int number = matr[0, j];
+        matr[0, j] = matr[lastrow, j];
+        matr[lastrow, j] = number;
+    }
+    return matr;
+}
+int [,] matrix = {
+{1, 2 ,3},
+{4, 5, 6},
+{7, 8 , 9},
+{10, 11 , 12}
+};
+PrintMatrix(matrix);
+PrintMatrix(ChangePlaces(matrix));
+// int [,] changedmatrix = ChangePlaces(matrix);
+// Console.WriteLine(changedmatrix);
